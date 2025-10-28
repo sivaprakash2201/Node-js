3423f81acae2578a0fb9d790ef5677fa49ed0a1d
sivaprakash2201 <siva99458@gmail.com>
2025-10-28 15:50:56 +0530

    app.js

--- Email Reminder System/starter/app.js
+++ Email Reminder System/starter/app.js
@@ -19,7 +19,10 @@ app.set("layout", "layout");
 
 // MongoDB Connection
 mongoose
-
+   .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -110,4 +113,5 @@ cron.schedule("* * * * *", async () => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
 });
